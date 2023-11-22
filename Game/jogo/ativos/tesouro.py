665466c03b937a37b65808cdb29941ccfa1ac6ad
amandfernandes <144744139+amandfernandes@users.noreply.github.com>
2023-11-22 10:09:26 -0300

    Delete Game directory

--- Game/jogo/ativos/tesouro.py
+++
@@ -1,9 +0,0 @@
-import random
-
-while True:
-    posicao = [random.randint(0, 5), random.randint(0, 5)]
-    if posicao != [0, 0]:
-        break
-
-if __name__ == "__main__":
-    print(posicao)
--- Game/jogo/personagens/monstro.py
+++
@@ -1,6 +0,0 @@
-import random
-
-class Monstro:
-    forca = random.randint(5,25)
-    defesa = random.randint(5,10)
-    vida = random.randint (10,100)
