4e24fca50b6bc2409922457c13072b163e8a403d
amandfernandes <amanda.fernandes2030@gmail.com>
2024-12-09 15:33:56 -0300

    Update mapa.py

--- AP2/jogo/ativos/mapa.py
+++ AP2/jogo/ativos/mapa.py
@@ -1,3 +1,60 @@
+import random
+
+class Map():
+    def __init__(self, width, height):
+        self.width = width
+        self.height = height
+        self.map =  self.createMap()
+
+    def createMap(self):
+        raise NotImplementedError("Método implementado nas subclasses.")
+    
+    def display(self):
+        for line in self.map:
+            print("  ".join(line))
+
+
+class MysteriousFlorest(Map):
+    def createMap(self):
+        map = []
+        for _ in range(self.height):
+            line = []
+            for _ in range(self.width):
+                if random.random() < 0.2:
+                    line.append("🌲")
+                else:
+                    line.append(".")
+            map.append(line)
+        return map
+    
+class UndergroundCave(Map):
+    def createMap(self):
+        map = []
+        for _ in range(self.height):
+            line = []
+            for _ in range(self.width):
+                if random.random() < 0.3:
+
+                    line.append("▒")
+                else:
+                    line.append(".")
+            map.append(line)
+        return map
+           
+
+if __name__ == "__main__":
+    florest = MysteriousFlorest(10,10)
+    florest.display()
+    print("-" * 20)
+    cave = UndergroundCave(20,10)
+    cave.display()
+    print("-" * 20)
+
+        
+
+
+
+"""
 def desenhar(personagem, tesouro):
     for y in range(10):
         for x in range(10):
@@ -8,8 +65,12 @@ def desenhar(personagem, tesouro):
             else:
                 print(".", end=" ")
         print()
+    
+def context():
+    print("Floresta")
 
 if __name__ == "__main__":
     desenhar([0, 0], [2, 4])
     print("-" * 12)
     desenhar([2, 1], [5, 0])
+"""
--- AP2/jogo/ativos/tesouro.py
+++ AP2/jogo/ativos/tesouro.py
@@ -1,5 +1,40 @@
 import random
 
+class Treasure:
+    def __init__(self, mapInstance):
+        self.map = mapInstance.map
+        self.width = mapInstance.width
+        self.height = mapInstance.height
+        self.treasurePosition = None
+
+    def placeTreasure(self):
+        while True:
+            x = random.randint(0, self.width - 1)
+            y = random.randint(0, self.height - 1)
+
+            if self.map[y][x] == ".":
+                self.treasurePosition = [x, y]
+                self.map[y][x] = "X" 
+                break
+
+    def getTeasurePosition(self):
+        return self.treasurePosition
+
+if __name__ == "__main__":
+    from mapa import MysteriousFlorest, UndergroundCave
+    florest = MysteriousFlorest(10,10)
+    treasure = Treasure(florest)
+    treasure.placeTreasure()
+    florest.display()
+    print(f"Tesouro posicionado em: {treasure.getTeasurePosition()}")
+    print("-" * 20)
+    cave = UndergroundCave(20,10)
+    treasure = Treasure(cave)
+    treasure.placeTreasure()
+    cave.display()
+    print(f"Tesouro posicionado em: {treasure.getTeasurePosition()}")
+
+"""
 class Tesouro:
     
      while True:
@@ -10,3 +45,4 @@ class Tesouro:
 
 if __name__ == "__main__":
     print(Tesouro.posicao)
+"""
